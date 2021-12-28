fda951e8d850a2c814d774e479829e546a14b55b
Joel Roske <joel.j.roske@gmail.com>
2021-12-28 14:16:39 -0600

    Installs, setup of folders and interfaces

---
+++ src/Component/MainPage/MainPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./MainPage.css";
+
+const MainPage: React.FC = () => {
+
+  return (
+    <div className="main-page">
+      <div>
+
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ src/Component/Stops/Stops.tsx
@@ -0,0 +1,9 @@
+import "./Stops.css";
+
+type Props = {};
+
+const Stops: React.FC<Props> = () => {
+  return <div data-testid="stops-list" className="stops-container"></div>;
+};
+
+export default Stops;
