aef3741df72e9fbcb04aec18b810f325cc21c05b
Joel Roske <joel.j.roske@gmail.com>
2021-12-28 15:32:13 -0600

    header and footer styles, unit and cypress integration tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, within, screen } from "@testing-library/react";
+import React from "react";
+import App from "./App";
+import MainPage from "./Components/MainPage/MainPage";
+import Stops from "./Components/Stops/Stops";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const stops = [
+  { place_code: "UNDP", description: "Union Depot" },
+  { place_code: "CNST", description: "Central Station" },
+];
+
+const selectedDirection = { direction_id: 0, direction_name: "Eastbound" };
+
+test("render the page title", () => {
+  const { getByText } = render(<App />);
+  const titleElement = getByText(/NexTrip/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test("render the main page select", () => {
+  const { getByText } = render(<MainPage />);
+  const titleElement = getByText(/Select a Route/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test(`has rendered a the items passed correctly`, () => {
+  const { getByRole } = render(
+    <Stops stops={stops} selectedDirection={selectedDirection} />
+  );
+  let listItems = getByRole("list");
+  expect(listItems.children.length).toEqual(2);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Wrapper } from "./App.styles";
 import "./App.styles.ts";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 import MainPage from "./Components/MainPage/MainPage";
 
 function App() {
   return (
     <Wrapper>
+      <Header />
       <MainPage />
+      <Footer />
     </Wrapper>
   );
 }
